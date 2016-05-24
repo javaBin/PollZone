bea438447c5f4e82a4a7499a29c50e8141170b90
Ingar Abrahamsen <ingar@abrahams1.com>
2016-05-24 18:52:22 +0200

    style: Change coding style in PollZone.ino

--- PollZone/PollZone.ino
+++ PollZone/PollZone.ino
@@ -1,9 +1,3 @@
-/*
- Name:		PollZone.ino
- Created:	11/23/2015 8:58:43 PM
- Author:	kristian
-*/
-
 #include <PubSubClient.h>
 #include <WiFiUdp.h>
 #include <WiFiServer.h>
@@ -45,186 +39,161 @@ int clientLoop = 0;
 // rotating leds
 int rotateSequence = 0;
 
-
-//BigButton red(D2, D3, "Red");
-//BigButton* red;
-
-
 // the setup function runs once when you press reset or power the board
 void setup() {
-	Serial.begin(115200);
-	delay(100);
+  Serial.begin(115200);
+  delay(100);
 
-	setUpButton(pinInGreen, pinOutGreen, &lastStateGreen);
-	setUpButton(pinInYellow, pinOutYellow, &lastStateYellow);
-	setUpButton(pinInRed, pinOutRed, &lastStateRed);
-	setUpResetButton(pinInReset, &lastStateRed);
+  setUpButton(pinInGreen, pinOutGreen, &lastStateGreen);
+  setUpButton(pinInYellow, pinOutYellow, &lastStateYellow);
+  setUpButton(pinInRed, pinOutRed, &lastStateRed);
+  setUpResetButton(pinInReset, &lastStateRed);
 
-	setupWifi();
-	//red = new BigButton(D2, D3, "Red");
-//	auto func = []() {Serial.println("From within func"); };
-//	func();
+  setupWifi();
 }
-
-
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-	if (!client.connected()) {
-		reconnect();
-	}
-	else {
-		clientLoop += 1;
-		if (clientLoop % 100 == 0) {
-			if (!client.loop())
-				Serial.println("PubSubClient loop failed");
-			clientLoop = 0;
-		}
-	}
+  if (!client.connected()) {
+    reconnect();
+  } else {
+    clientLoop += 1;
+    if (clientLoop % 100 == 0) {
+      if (!client.loop()) {
+        Serial.println("PubSubClient loop failed");
+      }
+      clientLoop = 0;
+    }
+  }
 
-	process(pinInGreen, pinOutGreen, &lastStateGreen, messageGreen);
-	process(pinInYellow, pinOutYellow, &lastStateYellow, messageYellow);
-	process(pinInRed, pinOutRed, &lastStateRed, messageRed);
-	processReset(pinInReset, &lastStateReset);
+  process(pinInGreen, pinOutGreen, &lastStateGreen, messageGreen);
+  process(pinInYellow, pinOutYellow, &lastStateYellow, messageYellow);
+  process(pinInRed, pinOutRed, &lastStateRed, messageRed);
+  processReset(pinInReset, &lastStateReset);
 }
 
 void setUpButton(uint8_t inPin, uint8_t outPin, int* oldState) {
-	pinMode(inPin, INPUT);
-	pinMode(outPin, OUTPUT);
-	digitalWrite(outPin, HIGH);
-	*oldState = 0;
+  pinMode(inPin, INPUT);
+  pinMode(outPin, OUTPUT);
+  digitalWrite(outPin, HIGH);
+  *oldState = 0;
 }
 
 void setUpResetButton(uint8_t inPin, int* oldState) {
-	pinMode(inPin, INPUT);
-	*oldState = 0;
+  pinMode(inPin, INPUT);
+  *oldState = 0;
 }
 
 void process(uint8_t inPin, uint8_t outPin, int* oldState, const char message[]) {
-	int buttonState = digitalRead(inPin);
-	if (buttonState != *oldState) {
+  int buttonState = digitalRead(inPin);
+  if (buttonState != *oldState) {
+    Serial.print(message);
+    Serial.print(" stateChange ");
+    Serial.print(*oldState);
+    Serial.print(" => ");
+    Serial.println(buttonState);
 
-	//		Serial.printf("last: %d now: %d\n", lastState, buttonState);
-	//		delay(500);
-		Serial.print(message);
-		Serial.print(" stateChange ");
-		Serial.print(*oldState);
-		Serial.print(" => ");
-		Serial.println(buttonState);
+    if (*oldState == 0) {
+      digitalWrite(outPin, LOW);
+    }	else {
+      digitalWrite(outPin, HIGH);
+      if (client.connected()) {
+        Serial.println("Publishing to MQTT topic " + topic + " value: " + message);
+        bool result = client.publish(topic.c_str(), message, true);
+        Serial.printf("Result: %s\n", result ? "true" : "false");
 
-	//Serial.printf("pinin: %d pinout: %d oldVal: %d newVal: %d message: %s\n", inPin, outPin, *oldState, buttonState, message);
-		if (*oldState == 0) {
-			digitalWrite(outPin, LOW);
-		}
-		else {
-			digitalWrite(outPin, HIGH);
-			if (client.connected()) {
-				Serial.println("Publishing to MQTT topic " + topic + " value: " + message);
-				bool result = client.publish(topic.c_str(), message, true);
-				Serial.printf("Result: %s\n", result ? "true": "false");
-       
-			}
-			else {
-				Serial.println("Not able to publish, client not connected");
-			}
-
-		}
-		*oldState = buttonState;
-		delay(100);
-	}
-
+      }	else {
+        Serial.println("Not able to publish, client not connected");
+      }
+    }
+    *oldState = buttonState;
+    delay(100);
+  }
 }
 
 void processReset(uint8_t inPin, int* oldState) {
-	int buttonState = digitalRead(inPin);
-	if (buttonState != *oldState) {
+  int buttonState = digitalRead(inPin);
+  if (buttonState != *oldState) {
+    Serial.print("Reset stateChange ");
+    Serial.print(*oldState);
+    Serial.print(" => ");
+    Serial.println(buttonState);
 
-		//		Serial.printf("last: %d now: %d\n", lastState, buttonState);
-		//		delay(500);
-		Serial.print("Reset stateChange ");
-		Serial.print(*oldState);
-		Serial.print(" => ");
-		Serial.println(buttonState);
-
-		*oldState = buttonState;
-	}
-
+    *oldState = buttonState;
+  }
 }
 
 void setupWifi() {
-	// We start by connecting to a WiFi network
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(wifi_ssid);
+  WiFi.begin(wifi_ssid, wifi_password);
 
-	Serial.println();
-	Serial.println();
-	Serial.print("Connecting to ");
-	Serial.println(wifi_ssid);
-	WiFi.begin(wifi_ssid, wifi_password);
-
-	while (WiFi.status() != WL_CONNECTED) {
-		delay(500);
-		Serial.print(".");
-		rotateLEDs(false);
-	}
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    rotateLEDs(false);
+  }
   rotateLEDs(true);
-	Serial.println("");
-	Serial.println("WiFi connected");
-	Serial.println("IP address: ");
-	Serial.println(WiFi.localIP());
-	Serial.println("MAC address: ");
-	mac = WiFi.macAddress();
-	Serial.println(mac);
-	topic = "/pollerbox/" + mac + "/vote";
-	Serial.println("Publishing on topic: " + topic);
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println("MAC address: ");
+  mac = WiFi.macAddress();
+  Serial.println(mac);
+  topic = "/pollerbox/" + mac + "/vote";
+  Serial.println("Publishing on topic: " + topic);
 
-	client.setServer(mqtt_server, 1883);
-
+  client.setServer(mqtt_server, 1883);
 }
 
 void reconnect() {
-	// Loop until we're reconnected
-	while (!client.connected()) {
-		Serial.print("Attempting MQTT connection...");
-		// Attempt to connect
-		if (client.connect(mac.c_str())) {
-			Serial.printf("connected! state: %d\n", client.state());
-		}
-		else {
-			Serial.print("failed, rc=");
-			Serial.print(client.state());
-			Serial.println(" try again in 5 seconds");
-			// Wait 5 seconds before retrying
-			delay(5000);
-		}
-	}
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(mac.c_str())) {
+      Serial.printf("connected! state: %d\n", client.state());
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
 }
 
 void rotateLEDs(boolean reset) {
-	rotateSequence++;
-	int active = rotateSequence % 3;
-	if(reset){
+  rotateSequence++;
+  int active = rotateSequence % 3;
+  if (reset) {
     digitalWrite(pinOutGreen, HIGH);
     digitalWrite(pinOutYellow, HIGH);
     digitalWrite(pinOutRed, HIGH);
-	}else{
-  	switch (active)
-  	{
-  	case 0:
-  		digitalWrite(pinOutGreen, HIGH);
-  		digitalWrite(pinOutYellow, LOW);
-  		digitalWrite(pinOutRed, LOW);
-  		break;
-  	case 1:
-  		digitalWrite(pinOutGreen, LOW);
-  		digitalWrite(pinOutYellow, HIGH);
-  		digitalWrite(pinOutRed, LOW);
-  		break;
-  	case 2:
-  		digitalWrite(pinOutGreen, LOW);
-  		digitalWrite(pinOutYellow, LOW);
-  		digitalWrite(pinOutRed, HIGH);
-  		break;
-  	default:
-  		break;
-  	}
-	}
+  } else {
+    switch (active) {
+      case 0:
+        digitalWrite(pinOutGreen, HIGH);
+        digitalWrite(pinOutYellow, LOW);
+        digitalWrite(pinOutRed, LOW);
+        break;
+      case 1:
+        digitalWrite(pinOutGreen, LOW);
+        digitalWrite(pinOutYellow, HIGH);
+        digitalWrite(pinOutRed, LOW);
+        break;
+      case 2:
+        digitalWrite(pinOutGreen, LOW);
+        digitalWrite(pinOutYellow, LOW);
+        digitalWrite(pinOutRed, HIGH);
+        break;
+      default:
+        break;
+    }
+  }
 }
+
