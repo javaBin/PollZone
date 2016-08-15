d32a51a8778d476b98db59459285bac964eb9f52
Trygve Laugstøl <trygvis@inamo.no>
2016-08-16 00:16:25 +0200

    o Refreshing PDFs from updated CMake tools. o Cleaning up header and PDF generation for v4 and adding for v5 board.

---
+++ PollZone/schematic-v5.h
@@ -0,0 +1,19 @@
+
+namespace schematic {
+
+static const int AD0 = D0;
+static const int LED_1 = D5;
+static const int LED_2 = D0;
+static const int LED_3 = D2;
+static const int LED_4 = D14;
+static const int LED_5 = D13;
+static const int SD2 = D9;
+static const int SD3 = D10;
+static const int SWITCH_1 = D16;
+static const int SWITCH_2 = D4;
+static const int SWITCH_3 = D12;
+static const int SWITCH_4 = D15;
+static const int SWITCH_5 = D3;
+
+} // namespace schematic
+
