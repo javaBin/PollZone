47503d7b497cc584f3bcffc2a85a0851158e194c
Trygve Laugstøl <trygvis@inamo.no>
2016-08-22 00:35:29 +0200

    o Removing default configuration and checking if it is set at compile time to prevent people from running without a configured build.

--- PollZone/config.h
+++ PollZone/config.h
@@ -1,5 +1,20 @@
 #pragma once
-#define wifi_ssid "<SSID>"
-#define wifi_password "<Password>"
-#define mqtt_server "<broker address>"
+
+// Remove comments and give a proper value for these settings:
+
+// #define wifi_ssid
+// #define wifi_password
+// #define mqtt_server
+
+#ifndef wifi_ssid
+#error You have to #define wifi_ssid
+#endif
+
+#ifndef wifi_password
+#error You have to #define wifi_password
+#endif
+
+#ifndef mqtt_server
+#error You have to #define mqtt_server
+#endif
 
