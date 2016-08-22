0d2032f461b202a3436558b5a1e1681ab8bdd4b3
Trygve Laugstøl <trygvis@inamo.no>
2016-08-22 16:04:47 +0200

    o Improved logo where the points actually meet each other.

--- PollZone/config.h
+++ PollZone/config.h
@@ -2,9 +2,9 @@
 
 // Remove comments and give a proper value for these settings:
 
-#define defaultWifiSsid
-#define defaultWifiPassword
-#define defaultMqttBroker
+// #define defaultWifiSsid
+// #define defaultWifiPassword
+// #define defaultMqttBroker
 
 #ifndef defaultWifiSsid
 #error You have to #define defaultWifiSsid
